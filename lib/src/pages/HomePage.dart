79f828a0c940f5ae685d9193c0e447b6eed324b7
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-05-18 00:06:32 -0400

    floating action button

--- lib/src/pages/HomePage.dart
+++ lib/src/pages/HomePage.dart
@@ -13,8 +13,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("QrReader",style: TextStyle(color: Colors.white),),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){},
+            padding: EdgeInsets.all(10.0),
+            icon: Icon(Icons.delete_forever)
+          )
+
+        ],
+      ),
       body: _callPage(currentIndex),
       bottomNavigationBar: _crearBottomNavigatorBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.zoom_out_map),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
     );
   }
 
